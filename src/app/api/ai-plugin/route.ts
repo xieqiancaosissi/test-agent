f1c9ca4dd0876988ec1737ca3ff5018699171951
naturexie <786281870@qq.com>
2025-04-18 11:42:44 +0800

    feat: update package.json

--- src/app/api/ai-plugin/route.ts
+++ src/app/api/ai-plugin/route.ts
@@ -1,4 +1,4 @@
-import { ACCOUNT_ID, PLUGIN_URL } from "@/app/config";
+import { ACCOUNT_ID } from "@/app/config";
 import { NextResponse } from "next/server";
 
 export async function GET() {
@@ -11,7 +11,7 @@ export async function GET() {
         },
         servers: [
             {
-                url: PLUGIN_URL,
+                url: "https://test-agent-rust.vercel.app",
             },
         ],
         "x-mb": {
